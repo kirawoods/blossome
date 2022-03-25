91781dfa2e573f03e380552a4d0189847b351190
kira-woods <kawoods826@gmail.com>
2022-03-24 17:08:36 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ function App() {
   return (
     <div className="App">
       <div className="Header">
-        <div className="Title">Blossome</div>
-        <div className="Subtitle">Attain your potential</div>
+        <div className="HeaderText">
+          <div className="Title">Blossome</div>
+          <div className="Subtitle">Attain your potential</div>
+        </div>
         <PointCounter pointTotal="200" />
       </div>
       <div className="ItemList">
-        <ListItem name="Feed Cats" points="50" />
+        <ListItem name="Feed Cats" points="5" />
+        <ListItem name="Play with Cats" points="10" />
+        <ListItem name="Cuddle Cats" points="3" />
       </div>
     </div>
   );
--- src/PointCounter.js
+++ src/PointCounter.js
@@ -3,7 +3,8 @@ import "./PointCounter.css";
 export function PointCounter(props) {
   return (
     <div className="PointCounter">
-      <div>{props.pointTotal}</div>
+      <img src={require("./coin.png")} alt="coin" className="coin-image"></img>
+      <div className="point-total">{props.pointTotal}</div>
     </div>
   );
 }
